72af6ad3abf5c1cd80655d98bc845db445436cdf
WeirdCuttlefish <WeirdCuttlefish@github.com>
2021-05-14 11:52:14 -0500

    Memory Initialized
    -STACK:
      -Push/Pop Frame
    -HEAP:
     -Copy variable

--- src/memory.h
+++ src/memory.h
@@ -6,16 +6,15 @@ class Memory{
                       eip, ebp, esp;      //CPU Cache
         std::uint8_t virtualMemory[65535];//[16.77696 mb]//RAM
         //EAX = Pointer but [EAX] = content @ EAX
-        //Maybe make my registers linked list nodes to make stack easier?
-
+        //EIP won't do anything as of right now
     public:
         Memory(){
             std::cout << "CPU + Memory Initialized" << "\n";
         }
     
-    //STACK STUFF
+    //General
     std::uint32_t valueAtLocation(int high){
-        //Converts from Little Endian!
+        //Modify to do any size!!!
         std::uint32_t value = 0;
         value |= virtualMemory[high - 3];
         value |= virtualMemory[high - 2] << 8 ;
@@ -24,23 +23,50 @@ class Memory{
         return value;
     }
 
+    void copy(std::uint32_t destination, std::uint32_t start){
+        virtualMemory[destination    ] = virtualMemory[start    ];
+        virtualMemory[destination - 1] = virtualMemory[start - 1];
+        virtualMemory[destination - 2] = virtualMemory[start - 2];
+        virtualMemory[destination - 3] = virtualMemory[start - 3];
+    }
+
+    //Stack -- NEEDS UPDATE!
     void pushStackFrame(){
-        //Convert to Little Endian
-        virtualMemory[esp] = ebp;
-        ebp = esp - 2;
-        esp = ebp - 2; 
+        copy(esp, ebp);   //Store old EBP 
+        ebp = esp-4;      //Frame pointer now at the old stack pointer-4
+        copy(ebp-4 , eax);//Kinda hacky solution -- will rewrite to make accurate
+        eax = ebp-4;
+        copy(ebp-8 , ebx);
+        ebx = ebp-8;
+        copy(ebp-12, ecx);
+        ecx = ebp-12;
+        copy(ebp-16, edx);
+        edx = ebp-16;
     }
 
     void popStackFrame(){
-        int returnLocation = virtualMemory[ebp+4];
-        eip = returnLocation;
+        ebp = valueAtLocation(ebp+4);
+        eax = ebp - 4;
+        ebx = ebp - 8;
+        ecx = ebp - 12;
+        edx = ebp - 16;
     }
+
     void push(int& x){
-        //Do I create a new stack frame for this ???
+        //ADD IN STACK UPDATE
     }
     void pop (int& x){
-        //pop(eax) -> eax = old eax
+        //ADD IN STACK UPDATE
     }
-    //HEAP STUFF
-    
+
+    //Heap -- NEEDS UPDATE TO MAKE DYNAMIC!!!
+    private:
+        int heapTop = -1;
+    public:
+        void saveVariable(std::uint8_t data[]){
+            heapTop = heapTop + sizeof(*data)/sizeof(std::uint8_t);
+            for(int i = 0; i <  sizeof(*data)/sizeof(std::uint8_t); i++){
+                virtualMemory[heapTop - i] = data[i];
+            }
+        }
 };
